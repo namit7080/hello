d68a6f5dbb71bc315c7a4c78a3759ab5e8ef0324
namit7080 <81043272+namit7080@users.noreply.github.com>
2022-08-25 12:51:04 +0530

    Update passport-jwt.js

--- config/passport-jwt.js
+++ config/passport-jwt.js
@@ -4,14 +4,18 @@ const user= require('../models/user');
 
 const Authenticate= async (req,res,next)=>{
 
+  
      try{
       
-       const token=req.cookies.token;
+       const token=req.body.cookies;
+       
        const verify=jwt.verify(token,'doubt');
-
+        
+       
      
        const rootUser= await user.findOne({_id:verify._id});
 
+       console.log(rootUser);
        if(!rootUser){
        
            return;
