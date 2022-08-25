26e8e6d03753b3957ba2e42acae0f05581280d81
namit7080 <81043272+namit7080@users.noreply.github.com>
2022-08-25 12:57:35 +0530

    Update singup.js

--- client/src/component/singup.js
+++ client/src/component/singup.js
@@ -29,7 +29,7 @@ export const Singup=()=>{
           e.preventDefault();
           
           const {username,email,profession,university,enrolled,courseyr,password}=user;
-          const response= await fetch('/register',{
+          const response= await fetch('http://34.221.190.159:7789/register',{
             method:"POST",
             headers:{
               "Content-Type": "application/json"
@@ -39,6 +39,7 @@ export const Singup=()=>{
             })
           });
 
+          console.log(response);
           
           const data= await response.json();
            
